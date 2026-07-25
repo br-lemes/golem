583530a318d7e549e360def93ad083dd378b9875
Breno Ramalho Lemes <breno@br-lemes.net>
2026-07-25 09:59:15 -0400

    feat: add commands for best prospecting and wisdom
    * Add best prospecting command to find optimal equipment
    * Add best wisdom command to find optimal equipment

---
+++ cmd/best_prospecting.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"github.com/br-lemes/golem/pkg/api"
+	"github.com/br-lemes/golem/pkg/completion"
+	"github.com/br-lemes/golem/pkg/console"
+	"github.com/br-lemes/golem/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var bestProspectingCmd = &cobra.Command{
+	Args:  cobra.ExactArgs(1),
+	Use:   "prospecting <name>",
+	Short: "Find the best equipment for prospecting",
+	Long: `Find the best equipment for prospecting
+
+Arguments:
+  name   Name of your character.`,
+	ValidArgsFunction: completion.CharacterName(1).Build(),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+		character, err := api.Characters(name)
+		if err != nil {
+			return err
+		}
+		items, err := utils.BestFinder(character, "",
+			map[string]int{"prospecting": 4, "inventory_space": 2})
+		if err != nil {
+			return err
+		}
+		console.Auto(items)
+		return nil
+	},
+}
+
+func init() {
+	bestCmd.AddCommand(bestProspectingCmd)
+}
