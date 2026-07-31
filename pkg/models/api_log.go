dfe5202450e9d0d916da16bd9c1e2af3d3ff038a
Breno Ramalho Lemes <breno@br-lemes.net>
2026-07-31 00:00:59 -0400

    perf: add GORM indexes to API log model
    * Add index tag to Method field
    * Add index tag to Path field
    * Add index tag to Status field

--- pkg/models/api_log.go
+++ pkg/models/api_log.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type APILog struct {
 	gorm.Model
-	Method   string
-	Path     string
+	Method   string `gorm:"index"`
+	Path     string `gorm:"index"`
 	Body     string
 	Response string
-	Status   int
+	Status   int `gorm:"index"`
 	Cooldown int
 }
